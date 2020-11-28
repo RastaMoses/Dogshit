26a580b103f3f1e66772ed72b42c9de14e3b9f6a
Tiffin007 <s-leischmidt@web.de>
2020-11-28 16:29:08 +0100

    Gate Opener script Added (Level 1)

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+    public float speed;
+    public Transform startPos;
+
+    Vector3 nextPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(transform.position == pos1.position)
+        {
+            nextPos = pos2.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+}
