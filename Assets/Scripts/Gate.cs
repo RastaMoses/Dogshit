1841af9f12d85284f0b7ba16cf8d0ab8800d27fe
Leon Schauer <leon@nyoucomer.de>
2020-11-29 04:18:22 +0100

    Added Delay for Gates and Started First Level

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -1,21 +1,49 @@
 ﻿using UnityEngine;
-
+using System.Collections;
 public class Gate : MonoBehaviour
 {
     public Transform pos1;
     public Transform pos2;
     public float speed;
     public Transform startPos;
+    [SerializeField] bool delayBeforeMove = false;
+    [SerializeField] float delay = 0f;
 
+    //State
+    bool addDelay;
     Vector3 nextPos;
     // Start is called before the first frame update
     void Start()
     {
         nextPos = startPos.position;
+        if (delayBeforeMove)
+        {
+            addDelay = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (addDelay)
+        {
+            StartCoroutine(MoveWithDelay());
+        }
+        else
+        {
+            Move();
+        }
+        
+    }
+    IEnumerator MoveWithDelay()
+    {
+        
+        yield return new WaitForSeconds(delay);
+        addDelay = false;
+        
+    }
+    
+    private void Move()
     {
         if (transform.position == pos1.position)
         {
@@ -27,5 +55,4 @@ public class Gate : MonoBehaviour
         }
         transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
     }
-
 }
