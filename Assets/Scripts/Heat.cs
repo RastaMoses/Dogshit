4aa6c4fc350d163009df5dc5d36f2702648a7be5
Leon Schauer <leon@nyoucomer.de>
2020-11-29 13:22:41 +0100

    Added Heater

---
+++ Assets/Scripts/Heat.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heat : MonoBehaviour
+{
+
+    [SerializeField] Color minColor;
+    [SerializeField] Color maxColor;
+    [SerializeField] float changeSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ColorFlash();
+    }
+
+    void ColorFlash()
+    {
+        GetComponent<SpriteRenderer>().color = Color.Lerp(minColor, maxColor,Mathf.PingPong(Time.time, changeSpeed));
+
+    }
+}
