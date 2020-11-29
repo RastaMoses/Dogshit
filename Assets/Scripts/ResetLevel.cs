2a0f766ba368ae3e6534c77673540d8254c7b3d5
Leon Schauer <leon@nyoucomer.de>
2020-11-29 11:49:15 +0100

    Reset Level on Hold R

---
+++ Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetLevel : MonoBehaviour
+{
+    [SerializeField] float pressKeyDuration = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reset());
+        }
+    }
+    IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(pressKeyDuration);
+        if (Input.GetKey(KeyCode.R))
+        {
+            GetComponent<SceneLoader>().ReloadLevel();
+        }
+        
+    }
+}
