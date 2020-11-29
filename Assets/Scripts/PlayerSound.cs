093b2cc53e2d83e557141bf58b4ecc2a65c9b6f2
Leon Schauer <leon@nyoucomer.de>
2020-11-29 14:16:39 +0100

    Added Kenney SFX and Player Audio Script

---
+++ Assets/Scripts/PlayerSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour
+{
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] List<AudioClip> jumpSFX;
+    [SerializeField] List<AudioClip> landSFX;
+
+
+
+    public void PlayJumpSFX()
+    {
+        int randomSFX = Random.Range(0, jumpSFX.Count);
+        var clip = jumpSFX[randomSFX];
+        AudioSource.PlayClipAtPoint(clip, gameObject.transform.position);
+    }
+
+    public void PlayDeathSFX()
+    {
+        
+        AudioSource.PlayClipAtPoint(deathSFX, gameObject.transform.position);
+    }
+    public void PlayLandSFX()
+    {
+        int randomSFX = Random.Range(0, landSFX.Count);
+        var clip = landSFX[randomSFX];
+        AudioSource.PlayClipAtPoint(clip, gameObject.transform.position);
+    }
+}
