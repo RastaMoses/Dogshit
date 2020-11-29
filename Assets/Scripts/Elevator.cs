6bfe5cee878c3d9dbf0484b09f6d3b09b6da74ac
Tiffin007 <s-leischmidt@web.de>
2020-11-29 04:30:37 +0100

    Level 2 started, Elevator added

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+    public float speed;
+    public Transform startPos;
+
+    Vector3 nextPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == pos1.position)
+        {
+            nextPos = pos2.position;
+        }
+        if (transform.position == pos2.position)
+        {
+            nextPos = pos1.position;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+}
