414ff123b9aca49d5779bb86f76f6092f3541429
Leon Schauer <leon@nyoucomer.de>
2020-12-02 00:49:08 +0100

    Added pause feature on ESC

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] PauseAnimation pauseBackgroundAnimation;
+    [SerializeField] TextMeshProUGUI pauseText;
+    bool paused;
+
+    private void Start()
+    {
+        pauseText.enabled = false;
+        pauseBackgroundAnimation = FindObjectOfType<PauseAnimation>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+            {
+                Unpause();
+            }
+            else
+            {
+                SetPause();
+            }
+            
+        }
+    }
+
+    private void SetPause()
+    {
+
+        pauseBackgroundAnimation.StartAnimation();
+        pauseText.enabled = true;
+        paused = true;
+        Time.timeScale = 0;
+    }
+
+    private void Unpause()
+    {
+        pauseText.enabled = false;
+        Time.timeScale = 1;
+        pauseBackgroundAnimation.StopAnimation();
+        paused = false;
+    }
+}
---
+++ Assets/Scripts/PauseAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseAnimation : MonoBehaviour
+{
+    [SerializeField] Color grungeTransparency;
+    Animator animator;
+    SpriteRenderer transparency;
+    private void Start()
+    {
+
+        animator = GetComponent<Animator>();
+        transparency = GetComponent<SpriteRenderer>();
+        transparency.color = new Color(0,0,0,0);
+    }
+
+    public void StartAnimation()
+    {
+        transparency.color = grungeTransparency;
+        animator.Play("Paused");
+    }
+
+    public void StopAnimation()
+    {
+        transparency.color = new Color(1,1,1,0);
+        animator.Play("Unpaused");
+    }
+}
